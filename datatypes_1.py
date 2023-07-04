a41f12fc08361e4ea9f69e6c4d22b8df7854b796
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-07-04 06:00:06 +0500

    updated

--- datatypes_1.py
+++ datatypes_1.py
@@ -7,3 +7,15 @@ print(type(x))
 x=10
 x=10/3
 print(x)
+
+x=["hello",2]
+
+x="Code Warrior"
+print(x[-1])
+
+#slicing x[start:stop:step]
+#stop is always minus 1 of the given index
+print(len(x))
+print(x[11])
+
+x=x[2:5]
