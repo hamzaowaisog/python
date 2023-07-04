d57e4e487fbbbef4249d8c47f6401c01798298d8
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-07-05 04:36:23 +0500

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,23 @@
+# =============================================================================
+#   IF-ELSE ELIF 
+# =============================================================================
+
+x=20
+
+if x>20:
+    print("x is greather than 5")
+    x=x+2
+elif x ==20:
+    print("X is equal to 20")
+else :
+    print("None condition is satisfied")
+    
+
+money = 100
+
+if money >10 and money<90:
+    print("Chotu chocolate")
+elif money == 100:
+    print("Bara choclate")
+else:
+    print("Save money")
