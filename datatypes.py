22dc1974df1ac48e850c9320af137ca8da1124a8
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-07-04 05:31:00 +0500

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+print("Hello World")
+x=10 ##Integer
+x="hello" ##String
+print(x)
+x=10.0 #Float
+y=[10,"hello",20.0] #List
+z={"key":"Hamza"} #Dictionary
+a=("hello",1,102.3) #Tuple
+b={"a","b"} #Sets
